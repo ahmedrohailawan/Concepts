11ca7767395ec0939a39bc98d1fb3ad4f4e9fe91
Ahmed Rohail Awan <ahmedrohailawanwaheedyousaf@gmail.com>
2023-04-05 01:02:25 +0500

    Add files via upload

---
+++ JavaScript/6. Functions/js.js
@@ -0,0 +1,28 @@
+console.log("this is the simple tutorial");
+
+
+function greet(name, greettext){
+    console.log(greettext + " " + name);
+    console.log(name + " is good boy");
+}
+let greettext1="good morning";
+let name  ="harry";
+let name1 ="ahmed";
+let name2 ="daniyal";
+let name3 ="asim";
+greet(name,greettext1);
+greet(name1,greettext1);
+greet(name2,greettext1);
+greet(name3,greettext1);
+
+
+function sum(a,b,c){
+    d= a+b+c;
+    return d;
+}
+let answer= sum(12,34,56);
+console.log(answer);
+// console.log(name+" is a good boy");
+// console.log(name1+" is a good boy");
+// console.log(name2+" is a good boy");
+// console.log(name3+" is a good girl");
